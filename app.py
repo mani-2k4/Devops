5bea09fa71db015e25d4bf13c3bd8742d479c1f2
manideep <mohanmanideep2k4@gmail.com>
2024-10-28 12:24:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hello, this is CICD pipeline"
+
+@app.route("/about")
+def about():
+    return "this is about page"
+
+
+
+if __name__ =='__main__' :
+    app.run(host = "0.0.0.0",port = 5000)
